cfad1e8a07cedd899b25fb0cac963e084048b8d0
Jim George <jgeorge@engr.colostate.edu>
2010-09-04 12:09:48 -0600

    Making use of the new drive open commands, added MOXA specific ioctls to use RS422 mode

--- tests/test-amc.c
+++ tests/test-amc.c
@@ -1,15 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#ifdef MOXA
+#include <moxadevice.h>
+#endif
 
 #include "src/amc.h"
 
-char serial_device[256] = "/dev/ttyUSB2";
+#ifdef MOXA
+#warning Using MOXA-specific ioctls to set RS422 mode
+#endif
+
+char serial_device[256] = "/dev/ttyM0";
 int baudrate = 115200;
 
 enum {
 	OPT_GETID = 256,
 	OPT_DEBUG,
+	OPT_PORT,
 	OPT_ENABLEBRIDGE,
 	OPT_QUICKSTOP,
 	OPT_RESETEVENTS,
@@ -18,10 +29,30 @@ enum {
 	OPT_SETIFACE,
 };
 
+int open_drive(struct amc_drive *drv, char *serial_device, int baudrate, int serial_mode, int *serial_fd)
+{
+	*serial_fd = amc_serial_open(serial_device, baudrate);
+
+	if (*serial_fd == -1) {
+		return -1;
+	}
+
+#ifdef MOXA
+	ioctl(*serial_fd, MOXA_SET_OP_MODE, &serial_mode);
+#endif
+
+	if (0 != amc_drive_new(drv, 0x3F, *serial_fd)) {
+		return -1;
+	}
+
+	return 0;
+}
+
 char *usage_string = 
 "Retrieve/control modbus registers on the WiBEX angle converter board\n"
 "Usage:\n"
 "--getid: Retrieve drive ID string and version numbers\n"
+"--port=<dev>: Set serial port device to dev\n"
 "--debug: Show serial comms debug messages\n"
 "--bridgestatus: Retrieve power bridge status\n"
 "--enablebridge[=n]: Enable the power bridge, n=0 disables, n=1 enables\n"
@@ -33,9 +64,9 @@ char *usage_string =
 
 static struct option opt_lst[] = {
 	{"debug", no_argument, 0, OPT_DEBUG},
+	{"port", required_argument, 0, OPT_PORT},
 
 	{"getid", no_argument, 0, OPT_GETID},
-
 	{"bridgestatus", no_argument, 0, OPT_BRIDGESTATUS},
 	{"enablebridge", optional_argument, 0, OPT_ENABLEBRIDGE},
 	{"quickstop", optional_argument, 0, OPT_QUICKSTOP},
@@ -50,6 +81,12 @@ int main(int argc, char *argv[])
 {
 	struct amc_drive *drv;
 	int opt_idx, opt_errors = 0, opt;
+	int serial_mode = 0;
+	int serial_fd;
+
+#ifdef MOXA
+	serial_mode = RS422_MODE;
+#endif
 
 	if (argc == 1) {
 		puts(usage_string);
@@ -58,17 +95,18 @@ int main(int argc, char *argv[])
 	
 	drv = malloc(sizeof(struct amc_drive));
 
-	if (0 != amc_drive_new(drv, serial_device, baudrate, 0x3F)) {
-		printf("Could not open serial device %s\n", serial_device);
-		return -1;
-	}
-	
-	if (0 > amc_get_access_control(drv)) {
-		printf("Could not get access to drive\n");
+	if (-1 == open_drive(drv, serial_device, baudrate, serial_mode, &serial_fd)) {
+		printf("Could not open %s\n", serial_device);
 	}
 
 	while (-1 != (opt = getopt_long(argc, argv, "dv", opt_lst, &opt_idx))) {
 		switch(opt) {
+		case OPT_PORT:
+			close(serial_fd);
+			if (-1 == open_drive(drv, serial_device, baudrate, serial_mode, &serial_fd)) {
+					printf("Could not open %s\n", serial_device);
+			}
+			break;
 		case OPT_GETID:
 			{
 			char buffer[256];
@@ -82,6 +120,7 @@ int main(int argc, char *argv[])
 			}
 			else {
 				printf("Could not retrieve product info\n");
+				return -1;
 			}
 			}
 			break;
@@ -162,81 +201,81 @@ int main(int argc, char *argv[])
 					printf("Could not read bridge status\n");
 					return -1;
 				}
-				printf("Bridge status: 0x%04X, Bridge: %s, DynBrake: %s, Shunt: %s\n"
-					"\tPosStop: %s, NegStop: %s, PosTorqueInhibit: %s, NegTorqueInhibit: %s\n"
-					"\tExtBrake: %s\n",
+				printf("Bridge status: 0x%04X \t[%c] Bridge Enabled\t[%c] DynBrake\n"
+						"\t\t[%c] Shunt Reg Enabled\t[%c] Positive Stop\t[%c] Negative Stop\n"
+						"\t\t[%c] PosTorqueInh\t[%c] NegTorqueInh\t[%c] Ext Brake\n",
 					bridge_status,
-					(bridge_status & AMC_BS_ENABLED) ? "Active" : "Disabled",
-					(bridge_status & AMC_BS_DYNBRAKE) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_SHUNT) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_POSSTOP) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_NEGSTOP) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_POSTORQUEINH) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_NEGTORQUEINH) ? "Enabled" : "Disabled",
-					(bridge_status & AMC_BS_EXTBRAKE) ? "Active" : "Inactive");
+					(bridge_status & AMC_BS_ENABLED) ? 'X' : ' ',
+					(bridge_status & AMC_BS_DYNBRAKE) ? 'X' : ' ',
+					(bridge_status & AMC_BS_SHUNT) ? 'X' : ' ',
+					(bridge_status & AMC_BS_POSSTOP) ? 'X' : ' ',
+					(bridge_status & AMC_BS_NEGSTOP) ? 'X' : ' ',
+					(bridge_status & AMC_BS_POSTORQUEINH) ? 'X' : ' ',
+					(bridge_status & AMC_BS_NEGTORQUEINH) ? 'X' : ' ',
+					(bridge_status & AMC_BS_EXTBRAKE) ? 'X' : ' ');
 
 				if (0 > amc_get_uint16(drv, 0x02, 0x01, &bridge_status)) {
 					printf("Could not read drive protection status\n");
 					return -1;
 				}
-				printf("Drive protection status: 0x%04X, Reset: %s, Internal Error: %s, Short Circuit: %s\n"
-					"\tOvercurrent: %s, Undervoltage: %s, Overvoltage: %s, Overtemp: %s\n",
+				printf("Drive protection status: 0x%04X\t[%c] Reset\t[%c] Internal Error\t[%c] Short Circuit\n"
+					"\t[%c] Overcurrent\t[%c] Undervoltage\t[%c] Overvoltage\t\t[%c] Overtemp\n",
 					bridge_status,
-					(bridge_status & AMC_PS_RESET) ? "Yes" : "No",
-					(bridge_status & AMC_PS_INTERROR) ? "Yes" : "No",
-					(bridge_status & AMC_PS_SHORTCKT) ? "Yes" : "No",
-					(bridge_status & AMC_PS_OVERCURRENT) ? "Yes" : "No",
-					(bridge_status & AMC_PS_UNDERVOLTAGE) ? "Yes" : "No",
-					(bridge_status & AMC_PS_OVERVOLTAGE) ? "Yes" : "No",
-					(bridge_status & AMC_PS_OVERTEMP) ? "Yes" : "No");
+					(bridge_status & AMC_PS_RESET) ? 'X' : ' ',
+					(bridge_status & AMC_PS_INTERROR) ? 'X' : ' ',
+					(bridge_status & AMC_PS_SHORTCKT) ? 'X' : ' ',
+					(bridge_status & AMC_PS_OVERCURRENT) ? 'X' : ' ',
+					(bridge_status & AMC_PS_UNDERVOLTAGE) ? 'X' : ' ',
+					(bridge_status & AMC_PS_OVERVOLTAGE) ? 'X' : ' ',
+					(bridge_status & AMC_PS_OVERTEMP) ? 'X' : ' ');
 
 				if (0 > amc_get_uint16(drv, 0x02, 0x02, &bridge_status)) {
 					printf("Could not read system protection status\n");
 					return -1;
 				}
-				printf("System protection status: 0x%04X, Parameter Restore Error: %s, Parameter Store Error: %s\n"
-					"\tMotor Overtemp: %s, Feedback Error: %s, Overspeed: %s, Communications Error: %s\n",
+				printf("System protection status: 0x%04X\t[%c] Param Restore Error\t[%c] Param Store Error\n"
+					"\t[%c] Motor Overtemp\t[%c] Feedback Error\t[%c] Overspeed\t[%c] Comms Error\n",
 					bridge_status,
-					(bridge_status & AMC_SS_RESTOREERR) ? "Yes" : "No",
-					(bridge_status & AMC_SS_STOREERR) ? "Yes" : "No",
-					(bridge_status & AMC_SS_MOTOROVERTEMP) ? "Yes" : "No",
-					(bridge_status & AMC_SS_FEEDBACKERROR) ? "Yes" : "No",
-					(bridge_status & AMC_SS_OVERSPEED) ? "Yes" : "No",
-					(bridge_status & AMC_SS_COMMERR) ? "Yes" : "No");
+					(bridge_status & AMC_SS_RESTOREERR) ? 'X' : ' ',
+					(bridge_status & AMC_SS_STOREERR) ? 'X' : ' ',
+					(bridge_status & AMC_SS_MOTOROVERTEMP) ? 'X' : ' ',
+					(bridge_status & AMC_SS_FEEDBACKERROR) ? 'X' : ' ',
+					(bridge_status & AMC_SS_OVERSPEED) ? 'X' : ' ',
+					(bridge_status & AMC_SS_COMMERR) ? 'X' : ' ');
 
 				if (0 > amc_get_uint16(drv, 0x02, 0x03, &bridge_status)) {
 					printf("Could not read drive status 1\n");
 					return -1;
-				}
-				printf("Drive status 1: 0x%04X, Log Missed: %s, Commanded Inhibit: %s, User Inhibit: %s\n"
-					"\tPositive Inhibit: %s, Negative Inhibit: %s, Current Limit: %s, Continuous Current Limit: %s\n"
-					"\tCurrent Loop Saturated: %s, Commanded Dynamic Brake: %s, User Dynamic Brake: %s, Shunt Reg: %s\n",
+				}				
+				printf("Drive status 1: 0x%04X\t[%c] Log Missed\t[%c] Commanded Inhibit\t[%c] User Inhibit\n"
+					"\t[%c] Pos Inhibit\t[%c] Neg Inhibit\t[%c] Current Limit\t[%c] Cont Current Limit\n"
+					"\t[%c] Current Loop Sat\t[%c] Cmd Dyn Brk\t[%c] User Dyn Brk\t[%c] Shunt Reg\n",
 					bridge_status,
-					(bridge_status & AMC_DS_LOGMISSED) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CMDINHIBIT) ? "Yes" : "No",
-					(bridge_status & AMC_DS_USERINHIBIT) ? "Yes" : "No",
-					(bridge_status & AMC_DS_POSINH) ? "Yes" : "No",
-					(bridge_status & AMC_DS_NEGINH) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CURRENTLIM) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CONTCURRENT) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CLSAT) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CMDDYNBRAKE) ? "Yes" : "No",
-					(bridge_status & AMC_DS_USERDYNBRAKE) ? "Yes" : "No",
-					(bridge_status & AMC_DS_SHUNTREG) ? "Active" : "Inactive");
+					(bridge_status & AMC_DS_LOGMISSED) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CMDINHIBIT) ? 'X' : ' ',
+					(bridge_status & AMC_DS_USERINHIBIT) ? 'X' : ' ',
+					(bridge_status & AMC_DS_POSINH) ? 'X' : ' ',
+					(bridge_status & AMC_DS_NEGINH) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CURRENTLIM) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CONTCURRENT) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CLSAT) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CMDDYNBRAKE) ? 'X' : ' ',
+					(bridge_status & AMC_DS_USERDYNBRAKE) ? 'X' : ' ',
+					(bridge_status & AMC_DS_SHUNTREG) ? 'X' : ' ');
 
 				if (0 > amc_get_uint16(drv, 0x02, 0x04, &bridge_status)) {
 					printf("Could not read drive status 1\n");
 					return -1;
 				}
-				printf("Drive status 2: 0x%04X, Zero Velocity: %s, At Command: %s, Velocity Following Error: %s\n"
-					"\tPositive Velocity Limit: %s, Negative Velocity Limit: %s, Command Profiler: %s\n",
+				printf("Drive status 2: 0x%04X\t[%c] Zero Velocity\t[%c] At Command\t[%c] Vel Following Error\n"
+					"\t[%c] Pos Velocity Limit\t[%c] Neg Velocity Limit\t[%c] Cmd Profiler\n",
 					bridge_status,
-					(bridge_status & AMC_DS_ZEROVEL) ? "Yes" : "No",
-					(bridge_status & AMC_DS_ATCMD) ? "Yes" : "No",
-					(bridge_status & AMC_DS_VELOCITYERR) ? "Yes" : "No",
-					(bridge_status & AMC_DS_POSVELOCITYLIM) ? "Yes" : "No",
-					(bridge_status & AMC_DS_NEGVELOCITYLIM) ? "Yes" : "No",
-					(bridge_status & AMC_DS_CMDPROFILER) ? "Active" : "Inactive");
+					(bridge_status & AMC_DS_ZEROVEL) ? 'X' : ' ',
+					(bridge_status & AMC_DS_ATCMD) ? 'X' : ' ',
+					(bridge_status & AMC_DS_VELOCITYERR) ? 'X' : ' ',
+					(bridge_status & AMC_DS_POSVELOCITYLIM) ? 'X' : ' ',
+					(bridge_status & AMC_DS_NEGVELOCITYLIM) ? 'X' : ' ',
+					(bridge_status & AMC_DS_CMDPROFILER) ? 'X' : ' ');
 			}
 			break;
 		case OPT_SETIFACE:
